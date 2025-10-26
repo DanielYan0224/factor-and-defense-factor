c4f950ee2a79755d5d954f47b067f30a0584364c
TLY <danielyan0224@gmail.com>
2025-10-26 01:09:53 -0500

    Initial commit after folder rename

---
+++ calculate_score.py
@@ -0,0 +1,17 @@
+#%%
+import pandas as pd
+import os
+
+from IPython.display import display
+import pandas as pd
+import numpy as np
+他
+import matplotlib.pyplot as plt
+from matplotlib import colors as mcolors
+
+from factor_and_defense_factor.expeact_score import get_expected_dataset
+
+df = get_expected_dataset()
+print(df.head())
+
+#%%
