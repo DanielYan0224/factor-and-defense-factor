6b97905558f158f4ec0ec5e7fbd47564c49f547f
qqaazz800624 <qqaazz800624@gmail.com>
2026-01-03 14:49:55 +0800

    update output file name

--- commands/parquet_transform.sh
+++ commands/parquet_transform.sh
@@ -3,4 +3,4 @@
 python scripts/parquet_transform.py \
     --data_dir "/neodata/open_dataset/mlb_data" \
     --save_dir "/neodata/open_dataset/mlb_data/preprocessed" \
-    --parquet_filename "truncated_data_with_rtheta.parquet"
+    --parquet_filename "truncated_data_with_rtheta_team.parquet"
