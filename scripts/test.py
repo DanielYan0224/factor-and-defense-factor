b8ad6075e6e33a6c73078db8880b3c38af684136
qqaazz800624 <qqaazz800624@gmail.com>
2026-01-03 14:51:16 +0800

    update

--- scripts/test.py
+++ scripts/test.py
@@ -9,19 +9,17 @@ import argparse
 #%%
 
 data_dir = '/neodata/open_dataset/mlb_data/preprocessed'
-parquet_filename = 'truncated_data_with_rtheta.parquet'
+parquet_filename = 'rtheta_prob_tbl.parquet'
 file_path = os.path.join(data_dir, parquet_filename)
 df = pd.read_parquet(file_path)
-
+df.head()
 
 #%%
 
 df.info()
 
-
 #%%
 
-df.head()
 
 
 
