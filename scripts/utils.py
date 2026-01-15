e81cc42207def357706b402e36437bf2335148ad
qqaazz800624 <qqaazz800624@gmail.com>
2026-01-15 17:51:08 +0800

    update prepare_regression_data and run_year_regression

--- scripts/utils.py
+++ scripts/utils.py
@@ -99,26 +99,28 @@ def prepare_regression_data(df: pd.DataFrame,
     event_weights = config.weights
     df_bip['real_metric'] = df_bip['events'].map(event_weights).fillna(0)
 
-    group_cols = ['game_year', 'home_team', 'pitcher_team', 'batter_team']
+    #group_cols = ['game_year', 'home_team', 'pitcher_team', 'batter_team']
+    group_cols = ['game_year', 'home_team', 'pitcher_team']
     
-    if 'game_pk' in df_bip.columns:
-        group_cols.append('game_pk')
-    else:
-        group_cols.append('game_date') 
+    # if 'game_pk' in df_bip.columns:
+    #     group_cols.append('game_pk')
+    # else:
+    #     group_cols.append('game_date') 
 
     agg_df = df_bip.groupby(group_cols).agg({
         'real_metric': 'sum',
         'expected_metric': 'sum',
-        'events': 'count' 
+        'events': 'count' # n_pd
     }).reset_index()
     
     agg_df.rename(columns={'events': 'weight', 'real_metric': 'sum_real', 'expected_metric': 'sum_exp'}, inplace=True)
     
-    agg_df = agg_df[agg_df['sum_exp'] > 1.0].copy()
+    #agg_df = agg_df[agg_df['sum_exp'] > 1.0].copy()
     
-    agg_df['log_ratio'] = np.log((agg_df['sum_real'] + 0.5) / (agg_df['sum_exp'] + 0.5))
-    agg_df['response'] = agg_df['sum_real'] - agg_df['sum_exp']
-    
+    # agg_df['log_ratio'] = np.log((agg_df['sum_real'] + 0.5) / (agg_df['sum_exp'] + 0.5))
+    # agg_df['response'] = agg_df['sum_real'] - agg_df['sum_exp']
+
+    agg_df['avg_residual'] = (agg_df['sum_real'] - agg_df['sum_exp']) / agg_df['weight']
     agg_df['park'] = agg_df['home_team']
     agg_df['defense'] = agg_df['pitcher_team']
     
@@ -126,57 +128,54 @@ def prepare_regression_data(df: pd.DataFrame,
 
 
 def run_year_regression(data, year):
-    
     data_yr = data[data['game_year'] == year].copy()
 
-    # Fit WLS
-    # Model: Y = Beta0 + Park + Defense
-    # Statsmodels drops one category (reference) for Park and Defense
-    model = smf.wls("response ~ C(park) + C(defense)", data=data_yr, weights=data_yr['weight'])
-    #res = model.fit()
-    res = model.fit_regularized(method='elastic_net', L1_wt=0.1, alpha=0.05)
+    model = smf.wls("avg_residual ~ C(park) + C(defense)", data=data_yr, weights=data_yr['weight'])
+    res = model.fit()
     
     params = res.params
-    
     all_parks = sorted(data_yr['park'].unique())
     all_defenses = sorted(data_yr['defense'].unique())
     
-    beta1 = {} 
-    beta2 = {} 
+    beta_park_raw = {} 
+    beta_def_raw = {} 
+    intercept_raw = params['Intercept']
     
-    beta0_raw = params['Intercept']
-    
-    # Fill Beta1 (Park)
-    # Statsmodels naming: C(park)[T.TeamName]
     for p in all_parks:
         key = f"C(park)[T.{p}]"
-        if key in params:
-            beta1[p] = params[key]
-        else:
-            beta1[p] = 0.0
+        beta_park_raw[p] = params.get(key, 0.0)
             
-    # Fill Beta2 (Defense)
     for d in all_defenses:
         key = f"C(defense)[T.{d}]"
-        if key in params:
-            beta2[d] = params[key]
-        else:
-            beta2[d] = 0.0
+        beta_def_raw[d] = params.get(key, 0.0)
             
-    mean_beta1 = np.mean(list(beta1.values()))
-    mean_beta2 = np.mean(list(beta2.values()))
+    mean_park = np.mean(list(beta_park_raw.values()))
+    mean_def = np.mean(list(beta_def_raw.values()))
     
-    beta1_adj = {k: v - mean_beta1 for k, v in beta1.items()}
-    beta2_adj = {k: v - mean_beta2 for k, v in beta2.items()}
-    beta0_adj = beta0_raw + mean_beta1 + mean_beta2
-    park_factors = {k: 100 * np.exp(v) for k, v in beta1_adj.items()}
-    defense_factors = {k: 100 * np.exp(v) for k, v in beta2_adj.items()}
+    beta_park_centered = {k: v - mean_park for k, v in beta_park_raw.items()}
+    beta_def_centered = {k: v - mean_def for k, v in beta_def_raw.items()}
+    adj_intercept = intercept_raw + mean_park + mean_def
+    
+    std_park = np.std(list(beta_park_centered.values()))
+    std_def = np.std(list(beta_def_centered.values()))
+    
+    park_indices = {}
+    for k, v in beta_park_centered.items():
+        z = v / std_park if std_park > 0 else 0
+        park_indices[k] = 100 + 20 * z
+        
+    defense_indices = {}
+    for k, v in beta_def_centered.items():
+        z = v / std_def if std_def > 0 else 0
+        defense_indices[k] = 100 - 20 * z
     
     return {
         'year': year,
-        'intercept': beta0_adj,
-        'park_factors': park_factors,
-        'defense_factors': defense_factors
+        'intercept': adj_intercept,
+        'park_factors': park_indices,
+        'defense_factors': defense_indices,
+        'park_std': std_park, 
+        'def_std': std_def
     }
 
 def get_local_image_b64(logos_dir, team_name):
